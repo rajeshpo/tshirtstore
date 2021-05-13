0bcfd5585e6dea145199eb45f5623b9d167478cd
CHITIKELA <rajesh.chitikela@dxc.com>
2021-05-13 13:24:21 +0530

    added build version

---
+++ build/precache-manifest.59f667b3ac406bfa0e186af3d17af082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c75adce554f069506af83985932b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63573188ff36bfd73a1a",
+    "url": "/static/css/2.a22538fc.chunk.css"
+  },
+  {
+    "revision": "d79862c10f271a5098a6",
+    "url": "/static/css/main.71e7aa14.chunk.css"
+  },
+  {
+    "revision": "63573188ff36bfd73a1a",
+    "url": "/static/js/2.aa331414.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.aa331414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79862c10f271a5098a6",
+    "url": "/static/js/main.dcc6019b.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
